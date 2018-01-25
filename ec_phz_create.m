9f72ac27950f2240aab4a209e10a550469f9057f
Gabe Nespoli <gabenespoli@gmail.com>
2018-01-25 14:33:38 -0500

    ec_phz_create.m: update for new phzlab and reanalysis in Jan 2018

--- ec_phz_create.m
+++ ec_phz_create.m
@@ -1,4 +1,4 @@
-function PHZ = ec_phz_create
+function PHZ = ec_phz_create(varargin)
 %% ec physio load phzfiles script
 % 
 %   1. pops up a dialog to select multiple acq-saved-as-mat files
@@ -7,12 +7,14 @@ function PHZ = ec_phz_create
 %   4. loads the marker channel and finds epoch times
 %   5. epochs the data
 %   6. saves the PHZ file.
+%   7. combines all PHZ files into a single one and returns it
 
 %% settings
+verbose = false;
 
 % 1. phz_create
 study = 'Hearing Aids SCL';
-filenameConvention = 'participant-session-group';
+filenameConvention = 'participant-group-session';
 
 datatype = 'scl';
 channelNumber = getChannelNumber(datatype);
@@ -30,7 +32,7 @@ dataUnits = getDataUnits(datatype);
 % 3. phz_filter [locut hicut notch]
 % EMG [10 450], RSP [0.5 1], SCR [0.05 0], PPG [0.5 3], ([hp lp])
 %                            SCL []
-filterCutoff = [0.05];
+filterCutoff = [0.05 0 0];
 filterOrder = 4;
 do_zerophase = false;
 
@@ -42,46 +44,83 @@ do_zerophase = false;
 % 9-2, remove 1st
 % 10-1 remove 28th
 % ? 12-2, 65 markers, looks like there are really only 62 good ones though
-markerChannel = 6;
-threshold = 0.1;
-markerBetween = 1; % in seconds (converted to samples below)
-plotMarkers = false;
-numMarkers = 64;
+% markerChannel = 6;
+% threshold = 0.1;
+% markerBetween = 1; % in seconds (converted to samples below)
+% plotMarkers = false;
+% numMarkers = 64;
 
 % 4b. phz_epoch
 extractWindow = [0 8]; % in seconds
 
 % 5. phz_save
-saveFolder       = fullfile('~','local','data','ec','phzfiles',datatype);
-epochTimesFolder = fullfile('~','local','data','ec','epoch_times_in_samples');
+saveFolder       = fullfile('~','local','ec','data','phzfiles',datatype);
+epochTimesFolder = fullfile('~','local','ec','data','epoch_times_in_samples');
+behavFolder      = fullfile('~','local','ec','data','behav');
 
 %% phzlab code
 
-% pop-up window to select files
-[files,folder] = uigetfile('.mat','MultiSelect','on');
-files = cellstr(files);
+if nargin > 0 && nargin < 3
+    for i = 1:length(varargin)
+        if ischar(varargin{i}) && ... % folder
+           isdir(varargin{i})
+            folder = varargin{i};
+        elseif (ischar(varargin{i}) && exist(varargin{i}, 'file')) || ...
+                iscell(varargin{i}) % file(s)
+            files = cellstr(varargin{i});
+        end
+    end
+else
+    % pop-up window to select files
+    [files,folder] = uigetfile('.mat','MultiSelect','on');
+end
+
+if ~exist('files', 'var') && ~isempty(folder)
+    d = dir(folder); % get files in folder
+    names = {d.name}; % get only filenames
+    ind = regexp(names, '^.*\.mat$'); % get only mat files
+    ind = ~cellfun(@isempty, ind); % convert cell to vector
+    files = names(ind);
+end
+
+if ~isempty(folder)
+    for i = 1:length(files)
+        files{i} = fullfile(folder, files{i});
+    end
+end
 
 % loop through files
 for i = 1:length(files)
-    
-    [~,filename] = fileparts(files{i});
-    currentFile = fullfile(folder,[filename,'.mat']);
+
+    if exist(files{i}, 'file')
+        [pathstr,filename] = fileparts(files{i});
+    else
+        pathstr = folder;
+    end
+    currentFile = fullfile(pathstr,[filename,'.mat']);
+    if ~exist(currentFile, 'file')
+        warning(['File ''', currentFile, ''' not found. Skipping...'])
+        continue
+    end
         
     PHZ = phz_create('file',currentFile,...
         'namestr',filenameConvention,...
         'channel',channelNumber,...
         'study',study,...
-        'datatype',datatype);
+        'datatype',datatype, ...
+        'verbose',verbose);
+
     PHZ.group = ec_group([char(PHZ.participant),'-',char(PHZ.session)]);
     PHZ.region = region;
     
-    PHZ = phz_transform(PHZ,conversionFactor);
+    PHZ = phz_transform(PHZ,conversionFactor,verbose);
     PHZ.units = dataUnits;
     
-%     PHZ = phz_filter(PHZ,filterCutoff,...
-%         'zerophase',do_zerophase,...
-%         'order',filterOrder);
-    
+    PHZ = phz_filter(PHZ,filterCutoff,...
+        'zerophase',do_zerophase,...
+        'order',filterOrder,...
+        'verbose',verbose);
+
 %     markerData = phz_create('file',currentFile,'channel',markerChannel);
 %     times = phzUtil_findAudioMarkers(markerData.data,...
 %         threshold,markerBetween * markerData.srate,...
@@ -93,11 +132,11 @@ for i = 1:length(files)
 %         char(PHZ.group),'-',...
 %         char(PHZ.session)];
     times = dlmread(fullfile(epochTimesFolder,[filename,'.txt']));
-    PHZ = phz_epoch(PHZ,extractWindow,times,'timeUnits','samples');
+    PHZ = phz_epoch(PHZ,extractWindow,times,'timeUnits','samples','verbose',verbose);
     
-    PHZ = insertTrialsAndBehaviouralResponses(PHZ);
+    PHZ = insertTrialsAndBehaviouralResponses(PHZ,behavFolder);
     
-    PHZ = phz_save(PHZ,fullfile(saveFolder,[filename,'.phz']));
+    phz_save(PHZ,fullfile(saveFolder,[filename,'.phz']));
     
     % save epoch times to text file
 %     fid = fopen(fullfile(epochTimesFolder,[filename,'.txt']),'w');
@@ -105,9 +144,13 @@ for i = 1:length(files)
 %     fclose(fid);
     
 end
+
+PHZ = phz_combine(saveFolder);
+
+
+
 end
 
-%% support functions
 function chan = getChannelNumber(datatype)
 switch lower(datatype)
     case 'zyg',         chan = 1;
@@ -137,11 +180,11 @@ switch lower(datatype)
 end
 end
 
-function PHZ = insertTrialsAndBehaviouralResponses(PHZ)
+function PHZ = insertTrialsAndBehaviouralResponses(PHZ,behavFolder)
 % load behav file
-load(fullfile('~','local','ec','data','behav',...
+load(fullfile(behavFolder,...
     [char(PHZ.participant),'-',char(PHZ.session),'.mat']),...
-    'answerString','respString','ACC','RT');
+    'answerString','respString','ACC','RT','stimFiles');
 
 % deal with exceptions (i.e., incorrect number of epochs)
 % 1-1, thresh 0.1 found 55 markers, removed 11th marker, 54 markers remain
@@ -166,12 +209,15 @@ switch [char(PHZ.participant),'-',char(PHZ.session)]
 end
 
 % add trial info and responses
-PHZ.meta.tags.trials = answerString(ind);
+PHZ.lib.tags.trials = answerString(ind);
 PHZ.resp.q1 = respString(ind);
 PHZ.resp.q1_acc = ACC(ind);
 PHZ.resp.q1_rt = RT(ind);
 
 % set trial order and plot colours
 PHZ.trials = {'angry','happy','sad','calm'};
-PHZ.meta.spec.trials = {'k-.','k-','k:','k--'};
+PHZ.lib.spec.trials = {'k-.','k-','k:','k--'};
+
+% save stimulus orders too
+PHZ.lib.tags.condition = stimFiles(ind);
 end
