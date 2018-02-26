53de3b42a35afb55d2a3a0199259ad22c3e9c11c
Gabe Nespoli <gabenespoli@gmail.com>
2018-02-26 11:42:36 -0500

    ec_getEpochTimes.m: misc updates

--- ec_getEpochTimes.m
+++ ec_getEpochTimes.m
@@ -1,6 +1,6 @@
-function times = ec_getEpochTimes(id)
+function [times, PHZ] = ec_getEpochTimes(id, plotMarkers)
+if nargin < 2, plotMarkers = true; end
 
-% 4a. phzUtil_findAudioMarkers
 % exceptions:
 % 1-1, thresh 0.1 found 55 markers, removed 11th marker, 54 markers remain
 % 2-1, thresh 0.1 found 66 markers, removed 1st and 2nd, 64 remain
@@ -8,28 +8,24 @@ function times = ec_getEpochTimes(id)
 % 9-2, remove 1st
 % 10-1 remove 28th
 % ? 12-2, 65 markers, looks like there are really only 62 good ones though
-% markerChannel = 6;
-% threshold = 0.1;
-% markerBetween = 1; % in seconds (converted to samples below)
-% plotMarkers = false;
-% numMarkers = 64;
 
 rawFolder = fullfile('~','local','ec','data','raw');
 suffix = '-times';
 
 filename = fullfile(rawFolder, [id, '.mat']);
 
-markerData = phz_create('file',     filename, ...
-                        'channel',  6);
+PHZ = phz_create('file',     filename, ...
+                 'channel',  6);
 
-times = phzUtil_findAudioMarkers(markerData.data, ...
-    threshold,      1 * markerData.srate, ...
-    'window',       [0 8] * markerData.srate, ...
+times = phzUtil_findAudioMarkers(PHZ.data, ...
+    0.1, ...            % threshold
+    1 * PHZ.srate, ...  % time between
     'numMarkers',   64, ...
-    'plotMarkers',  false);
+    'plotMarkers',  plotMarkers);
+times = times';
 
 timesfile = fullfile(rawFolder, [id, suffix, '.txt']);
-if exist(timesfile, file)
+if exist(timesfile, 'file')
     fprintf('File exists: %s\n', timesfile)
     s = input('[o]verwrite or [c]ancel: ', 's');
     if ~strcmpi(s, 'o')
@@ -39,7 +35,7 @@ if exist(timesfile, file)
 end
 
 fid = fopen(timesfile, 'wt');
-fprintf(fid, '%i\n', PHZ.proc.epoch.times);
+fprintf(fid, '%i\n', times);
 fclose(fid);
 
 end
