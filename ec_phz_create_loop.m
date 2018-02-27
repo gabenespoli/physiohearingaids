9a2f89722f43a7900dfebf21429a6eab07e7682f
Gabe Nespoli <gabenespoli@gmail.com>
2018-02-27 09:26:34 -0500

    ec_phz_create_loop.m: working well now

--- ec_phz_create_loop.m
+++ ec_phz_create_loop.m
@@ -1,10 +1,8 @@
-function PHZ = ec_phz_create_loop(ids, datatype)
+function PHZ = ec_phz_create_loop(ids, datatype, force, verbose)
 
-if nargin < 2, datatype = 'scl'; end
-
-saveFolder = fullfile('~','local','ec','data','phzfiles',datatype);
-force = 2;
-verbose = true;
+if nargin < 2 || isempty(datatype), datatype = 'scl'; end
+if nargin < 3 || isempty(force),    force = 0; end
+if nargin < 4 || isempty(verbose),  verbose = true; end
 
 if nargin < 1
     ids = ec_datafiles;
@@ -12,6 +10,8 @@ else
     ids = cellstr(ids);
 end
 
+saveFolder = fullfile('~','local','ec','data','phzfiles',datatype);
+filenames = cell(1,length(ids));
 for i = 1:length(ids)
     id = ids{i};
     fprintf('Processing ec id %i/%i: ''%s''\n', i, length(ids), id)
@@ -19,11 +19,16 @@ for i = 1:length(ids)
     PHZ = ec_phz_create(id, datatype, false);
 
     fname = fullfile(saveFolder, [id, '.phz']);
-    phz_save(PHZ, fname, verbose, force);
+    if isempty(fname), return, end
+    PHZ = phz_save(PHZ, fname, verbose, force);
 
+    filenames{i} = PHZ.lib.filename;
 end
 
-PHZ = phz_combine(saveFolder);
+PHZ = phz_combine(filenames);
+
+% set some plotting parameters
+PHZ.group = {'normal', 'impaired', 'aided'};
+PHZ.lib.spec.group = {'k-', 'k', 'k--'};
 
 end
-
