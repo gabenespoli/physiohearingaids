5cef19c8d366f6d2cd3b45ba0140d4076042fa51
Gabe Nespoli <gabenespoli@gmail.com>
2018-02-27 10:14:25 -0500

    ec_writetables.m: pull out savename so we can more easily save or not

--- ec_writetables.m
+++ ec_writetables.m
@@ -1,28 +1,32 @@
-function ec_writetables
+function [meanscl, rt, acc] = ec_writetables
 
+% settings
 suffix = '_with_stimulus_name';
-
-PHZ = phz_load(fullfile('~','local','ec','data','phzfiles',[suffix, '.phz']));
+savename = [];
 savedir = fullfile('~','projects','archive','2014','ec','stats');
 
+% load and preprocess data
+PHZ = phz_load(fullfile('~','local','ec','data','phzfiles','scl.phz'));
 scl   = PHZ;
 scl   = phz_blsub(scl, 'baseline');
 scl   = phz_reject(scl, 0.05);
 behav = PHZ;
 behav = phz_subset(behav, PHZ.resp.q1_rt < 7);
 
+% calls to phz_writetable
 region = [1 5];
-phz_writetable(scl,...
+savename = fullfile(savedir, ['mean_', phzUtil_num2strRegion(region, '-'), suffix, '.csv']);
+meanscl = phz_writetable(scl,...
     'region',   region,...
     'feature',  'mean',...
-    'save',     fullfile(savedir, ['mean_',...
-                                   phzUtil_num2strRegion(region, '-'),...
-                                   suffix, '.csv']));
+    'save',     savename);
 
-phz_writetable(behav,...
+savename = fullfile(savedir, ['rt', suffix, '.csv']);
+rt = phz_writetable(behav,...
     'feature',  'rt',...
-    'save',     fullfile(savedir, ['rt', suffix, '.csv']));
+    'save',     savename);
 
-phz_writetable(behav,...
+savename = fullfile(savedir, ['acc', suffix, '.csv']);
+acc = phz_writetable(behav,...
     'feature',  'acc',...
-    'save',     fullfile(savedir, ['acc', suffix, '.csv']));
+    'save',     savename);
