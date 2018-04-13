1145286958460128c8c198d33fadcbd3420821cb
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-13 13:22:05 -0400

    ec_phz_create_loop.m: get all ids if var is empty

--- ec_phz_create_loop.m
+++ ec_phz_create_loop.m
@@ -1,14 +1,13 @@
 function PHZ = ec_phz_create_loop(ids, datatype, force, verbose)
 
-if nargin < 2 || isempty(datatype), datatype = 'scl'; end
-if nargin < 3 || isempty(force),    force = 0; end
-if nargin < 4 || isempty(verbose),  verbose = true; end
-
-if nargin < 1
+if nargin < 1 || isempty(ids)
     ids = ec_datafiles;
 else
     ids = cellstr(ids);
 end
+if nargin < 2 || isempty(datatype), datatype = 'scl'; end
+if nargin < 3 || isempty(force),    force = 0; end
+if nargin < 4 || isempty(verbose),  verbose = true; end
 
 saveFolder = fullfile('~','local','ec','data','phzfiles',datatype);
 filenames = cell(1,length(ids));
