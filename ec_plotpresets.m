dceb7ca0c1ef688e4ccfc0927f605489c6e45f26
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-13 08:50:20 -0400

    ec_plotpresets.m: add sigstar options

--- ec_plotpresets.m
+++ ec_plotpresets.m
@@ -19,12 +19,20 @@ PHZ.lib.plots.(name).title          = false;
 PHZ.lib.plots.(name).simpleytitle   = true;
 PHZ.lib.plots.(name).fontsize       = fontsize;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 3 ', name, ext]);
+% PHZ.lib.plots.(name).sigstar        = { {{'normal', 'impaired'}, ...
+%                                          {'impaired', 'aided'}, ...
+%                                          {'normal', 'aided'}}, ...
+%                                         [0.07991 0.2 0.001778], 1, fontsize };
 
 name = 'rt';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.acc;
 PHZ.lib.plots.(name).feature        = 'rt';
 PHZ.lib.plots.(name).fontsize       = fontsize;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 5 ', name, ext]);
+% PHZ.lib.plots.(name).sigstar        = { {{'normal', 'impaired'}, ...
+%                                          {'impaired', 'aided'}, ...
+%                                          {'normal', 'aided'}}, ...
+%                                         [9.219e-6 0.7759 5.578e-6], 1, fontsize };
 
 name = 'sclMean';
 PHZ.lib.plots.(name).transform      = 1/5; % convert to same units as first time preprocessing 
@@ -38,24 +46,31 @@ PHZ.lib.plots.(name).title          = false;
 PHZ.lib.plots.(name).simpleytitle   = true;
 PHZ.lib.plots.(name).fontsize       = fontsize;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 7 ', name, ext]);
+% PHZ.lib.plots.(name).sigstar        = { {{'normal', 'impaired'}, ...
+%                                          {'impaired', 'aided'}, ...
+%                                          {'normal', 'aided'}}, ...
+%                                         [0.5184 0.7274 0.619], 1, fontsize };
 
 name = 'accByEmo';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.acc;
 PHZ.lib.plots.(name).summary        = {'group', 'trials'};
 PHZ.lib.plots.(name).fontsize       = fontsize + 4;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 4 ', name, ext]);
+PHZ.lib.plots.(name).sigstar        = [];
 
 name = 'rtByEmo';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.rt;
 PHZ.lib.plots.(name).summary        = {'group', 'trials'};
 PHZ.lib.plots.(name).fontsize       = fontsize + 4;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 6 ', name, ext]);
+PHZ.lib.plots.(name).sigstar        = [];
 
 name = 'sclMeanByEmo';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.sclMean;
 PHZ.lib.plots.(name).summary        = {'group','trials'};
 PHZ.lib.plots.(name).fontsize       = fontsize + 4;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 8 ', name, ext]);
+PHZ.lib.plots.(name).sigstar        = [];
 
 name = 'sclByEmo';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.sclMean;
@@ -63,5 +78,6 @@ PHZ.lib.plots.(name).feature        = '';
 PHZ.lib.plots.(name).smooth         = true;
 PHZ.lib.plots.(name).fontsize       = fontsize + 4;
 PHZ.lib.plots.(name).filename       = fullfile(folder, ['Figure 12 ', name, ext]);
+PHZ.lib.plots.(name).sigstar        = [];
 
 end
