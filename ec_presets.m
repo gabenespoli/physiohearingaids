bf8030faf3737c6fc63cf35d1fb76cd8decc8ccf
Gabe Nespoli <gabenespoli@gmail.com>
2018-04-16 11:55:48 -0400

    ec_presets.m: major update of organization

--- ec_presets.m
+++ ec_presets.m
@@ -4,15 +4,25 @@ function PHZ = ec_presets(PHZ)
 plotFolder = fullfile('~','projects','ec','plots'); % where to save plots
 tableFolder = fullfile('~','projects','ec','stats'); % where to save plots
 ext = '.eps'; % filetype for plots
+
 PHZ.group = {'normal', 'impaired', 'aided'}; % set order for plotting
 PHZ.lib.spec.group = {'k-', 'k:', 'k--'};
 tooSlowRT = 'PHZ.resp.q1_rt < 7';
 
-% plots
-plotDefaults.fontsize       = 18;
-plotDefaults.simpleytitle   = true;
-plotDefaults.pretty         = true;
-plotDefaults.save           = false;
+sclProcessing.reject        = 1;
+sclProcessing.blsub         = [0 1];
+sclProcessing.region        = [1 5];
+sclProcessing.feature       = 'mean';
+
+sclDefaults                 = sclProcessing;
+sclDefaults.fontsize        = 18;
+sclDefaults.title           = false;
+sclDefaults.simpleytitle    = true;
+sclDefaults.pretty          = true;
+sclDefaults.save            = false;
+sclDefaults.sigstar         = 'unpaired';
+
+plotDefaults = rmfield(sclDefaults, fieldnames(sclProcessing));
 
 %% discard previous presets
 fields = {'plots', 'tables'};
@@ -37,79 +47,52 @@ PHZ.lib.tables.(name).save          = false;
 PHZ.lib.tables.(name).subset        = tooSlowRT;
 
 name = 'sclMean';
-PHZ.lib.tables.(name).feature       = 'mean';
-PHZ.lib.tables.(name).region        = [1 5];
+PHZ.lib.tables.(name)               = sclProcessing;
 PHZ.lib.tables.(name).filename      = fullfile(tableFolder, ['mean_1-5', '.csv']);
 PHZ.lib.tables.(name).save          = false;
-PHZ.lib.tables.(name).reject        = 1;
-PHZ.lib.tables.(name).blsub         = [0 1];
 
 %% plots: acc
 name = 'acc';
-PHZ.lib.plots.(name).feature        = 'acc';
 PHZ.lib.plots.(name)                = plotDefaults;
-PHZ.lib.plots.(name).title          = false;
+PHZ.lib.plots.(name).feature        = 'acc';
 PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 3 ', name, ext]);
 PHZ.lib.plots.(name).subset         = tooSlowRT;
 PHZ.lib.plots.(name).summary        = 'group';
-% PHZ.lib.plots.(name).sigstar        = { {{'normal', 'impaired'}, ...
-%                                          {'impaired', 'aided'}, ...
-%                                          {'normal', 'aided'}}, ...
-%                                         [0.07991 0.2 0.001778], 1, fontsize };
 
 name = 'accByEmo';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.acc;
 PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 4 ', name, ext]);
 PHZ.lib.plots.(name).fontsize       = plotDefaults.fontsize + 4;
 PHZ.lib.plots.(name).summary        = {'group', 'trials'};
-PHZ.lib.plots.(name).sigstar        = [];
 
 %% plots: rt
 
 name = 'rt';
-PHZ.lib.plots.(name).feature        = 'rt';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.acc;
+PHZ.lib.plots.(name).feature        = 'rt';
 PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 5 ', name, ext]);
-% PHZ.lib.plots.(name).sigstar        = { {{'normal', 'impaired'}, ...
-%                                          {'impaired', 'aided'}, ...
-%                                          {'normal', 'aided'}}, ...
-%                                         [9.219e-6 0.7759 5.578e-6], 1, fontsize };
 
 name = 'rtByEmo';
-PHZ.lib.plots.(name)                = PHZ.lib.plots.rt;
+PHZ.lib.plots.(name)                = PHZ.lib.plots.accByEmo;
+PHZ.lib.plots.(name).feature        = 'rt';
 PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 6 ', name, ext]);
-PHZ.lib.plots.(name).summary        = {'group', 'trials'};
-PHZ.lib.plots.(name).fontsize       = plotDefaults.fontsize + 4;
-PHZ.lib.plots.(name).sigstar        = [];
 
 %% plots: scl
 name = 'sclMean';
-PHZ.lib.plots.(name).feature        = 'mean';
-PHZ.lib.plots.(name).region         = [1 5];
-PHZ.lib.plots.(name).summary        = 'group';
-PHZ.lib.plots.(name)                = plotDefaults;
-PHZ.lib.plots.(name).title          = false;
+PHZ.lib.plots.(name)                = sclDefaults;
 PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 7 ', name, ext]);
-PHZ.lib.plots.(name).blsub          = [0 1];
-PHZ.lib.plots.(name).reject         = 1;
-% PHZ.lib.plots.(name).sigstar        = { {{'normal', 'impaired'}, ...
-%                                          {'impaired', 'aided'}, ...
-%                                          {'normal', 'aided'}}, ...
-%                                         [0.5184 0.7274 0.619], 1, fontsize };
+PHZ.lib.plots.(name).summary        = 'group';
 
 name = 'sclMeanByEmo';
 PHZ.lib.plots.(name)                = PHZ.lib.plots.sclMean;
 PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 8 ', name, ext]);
-PHZ.lib.plots.(name).fontsize       = plotDefaults.fontsize + 4;
 PHZ.lib.plots.(name).summary        = {'group','trials'};
-PHZ.lib.plots.(name).sigstar        = [];
-
-name = 'sclByEmo';
-PHZ.lib.plots.(name)                = PHZ.lib.plots.sclMean;
-PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 12 ', name, ext]);
 PHZ.lib.plots.(name).fontsize       = plotDefaults.fontsize + 4;
+
+name = 'sclByEmo'; % time-series plot
+PHZ.lib.plots.(name)                = PHZ.lib.plots.sclMeanByEmo;
+PHZ.lib.plots.(name).filename       = fullfile(plotFolder, ['Figure 12 ', name, ext]);
 PHZ.lib.plots.(name).feature        = '';
 PHZ.lib.plots.(name).smooth         = true;
-PHZ.lib.plots.(name).sigstar        = [];
 
 end
